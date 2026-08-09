49f971e481499bc52cf652c0d6bd1916a0a5030a
YashAggarwal01 <mr.yashgamerzyt@gmail.com>
2026-08-09 23:28:42 +0530

    prettier

--- Hospital Management/medicalrecord.model.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose"
-
-const medicalrecordSchema = new mongoose.Schema({
-
-},{timestamps:true})
-
-
-export const MedicalRecord = mongoose.model("MedicalRecord",medicalrecordSchema)
